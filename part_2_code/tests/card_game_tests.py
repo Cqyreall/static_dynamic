10bf48c539274c0e0b1f7ecd90ff8f4a0f071e3f
Cyril Ishabiyi <cyrilishabiyi@cyrils-air.home>
2020-10-05 16:47:33 +0100

    last function done

--- part_2_code/tests/card_game_tests.py
+++ part_2_code/tests/card_game_tests.py
@@ -5,16 +5,20 @@ from src.card_game import CardGame
 class TestCardGame(unittest.TestCase):
 
     def setUp(self):
-        self.card1 = Card('hearts', 8)
-        self.card2 = Card('spade', 1)
-        self.card3 = Card('diamonds', 9)
+        card1 = Card('hearts', 8)
+        card2 = Card('spade', 1)
+        card3 = Card('diamonds', 9)
+        self.cards = [card1, card2, card3]
    
     def test_card_has_ace(self):
-        self.assertEqual(False, CardGame.check_for_ace(self, self.card1))
-        self.assertEqual(True, CardGame.check_for_ace(self, self.card2))
+        self.assertEqual(False, CardGame.check_for_ace(self, self.cards[0]))
+        self.assertEqual(True, CardGame.check_for_ace(self, self.cards[1]))
     
     def test_card1_has_highest_value(self):
-        self.assertEqual(self.card1, CardGame.highest_card(self, self.card1, self.card2))
+        self.assertEqual(self.cards[0], CardGame.highest_card(self, self.cards[0], self.cards[1]))
     
     def test_card2_has_highest_value(self):
-        self.assertEqual(self.card3, CardGame.highest_card(self, self.card1, self.card3))
+        self.assertEqual(self.cards[2], CardGame.highest_card(self, self.cards[0], self.cards[2]))
+
+    def test_total(self):
+        self.assertEqual('You have a total of 18', CardGame.cards_total(self, self.cards))
